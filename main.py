8b026ab6f1140dbf17b0bd386da5e829d910eaaf
Felipe Pereira De Britto <felipepereiradebritto08@gmail.com>
2024-09-03 20:10:31 -0300

    oi

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#test kkk
+#aperte  "alt", "z"
+
+
+#test kkkVocê deverá criar uma classe base chamada BombaCombustivel para representar uma bomba de combustível em um posto. Além disso, crie subclasses específicas para diferentes tipos de combustível, como BombaEtanol e BombaGasolina. As subclasses herdam métodos da classe base para realizar abastecimentos por valor e por litro, além dos construtores, getters e setters padrões do Python. A classe base terá atributos para o valor do litro do combustível e a quantidade disponível na bomba. A solução deve garantir que não sejam realizados abastecimentos com capacidade maior do que a quantidade disponível na bomba. Os valores de valor_litro e quantidade_disponivel serão definidos no momento da instanciação dos objetos, mas a classe BombaGasolina oferece também gasolina com aditivo sem causar alterações extras na quantidade_disponivel de gasolina na bomba pois o dispositivo que adiciona aditivo vem de um outro local.
+
+#valor_litro: Representa o preço por litro do combustível;
+#quantidade_disponivel: Representa a quantidade de combustível disponível na bomba.
